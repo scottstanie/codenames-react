952b077b87f286e6cb906c4241002a752caaa119
scott <scott@cogolabs.com>
2016-08-17 20:31:27 -0400

    clue text inputs seem to show up

--- app/inputbutton.js
+++ app/inputbutton.js
@@ -20,7 +20,8 @@ export default class InputButton extends Component {
   render() {
     // console.log('inputbutton render', this.state);
     let { isGiver, isGuesser } = this.props;
-    console.log('inputbuttor render', this.props);
+    // console.log('inputbuttor render', this.props);
+    console.log('inputbuttor render state', this.state);
     let inputButton;
     if (isGuesser) {
                                       // onPress={() => _handleGameClick(navigator, rowData.id)}>
@@ -30,27 +31,33 @@ export default class InputButton extends Component {
     } else if (isGiver) {
                                       // onPress={() => _handleGameClick(navigator, rowData.id)}>
       inputButton = <View style={styles.giveWrap}>
-                      <View>
+
                         <TextInput
-                          style={{height: 40, width:200, borderColor: 'gray', borderWidth: 1}}
+                          style={{height: 40, width:100, borderColor: 'gray', borderWidth: 1}}
                           onChangeText={(text) => this.setState({clue: text})}
+                          value={this.state.clue}
                           />
-                        <Picker selectedValue={this.state.cardCount}
-                                  onValueChange={(count) => this.setState({cardCount: count})}>
-                          <Picker.Item label="zero" value="0" />
-                          <Picker.Item label="one" value="1" />
-                          <Picker.Item label="two" value="2" />
-                          <Picker.Item label="three" value="3" />
-                          <Picker.Item label="four" value="4" />
-                          <Picker.Item label="five" value="5" />
-                        </Picker>
-                      </View>
+                        <TextInput style={{height: 40, width:100,borderColor: 'gray', borderWidth: 1}}
+                          keyboardType="phone-pad"
+                          onChangeText={(text) => this.setState({cardCount: text})}
+                          value={this.state.cardCount}
+                        />
+
 
                       <Button style={styles.button} textStyle={styles.buttonText} >
                         Give Clue
                       </Button>
                       </View>
     }
+                        // <Picker selectedValue={this.state.cardCount}
+                        //           onValueChange={(count) => this.setState({cardCount: count})}>
+                        //   <Picker.Item label="zero" value="0" />
+                        //   <Picker.Item label="one" value="1" />
+                        //   <Picker.Item label="two" value="2" />
+                        //   <Picker.Item label="three" value="3" />
+                        //   <Picker.Item label="four" value="4" />
+                        //   <Picker.Item label="five" value="5" />
+                        // </Picker>
 
     // let { _handleGameClick } = this.props;
     return (
@@ -69,6 +76,7 @@ const styles = StyleSheet.create({
   },
   giveWrap: {
     flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   buttonWrap: {
     position: 'absolute',
